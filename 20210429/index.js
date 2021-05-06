b50f446641b1595dd7dad558945a6916f60ef913
C108156112 <C108156112@nkust.edu.tw>
2021-05-06 11:22:44 +0800

    index

---
+++ 20210429/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import LoginControl from './App9.js';
+
+
+ReactDOM.render(<LoginControl />, document.getElementById('root'));
+/*
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import App from './App8.js';
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));*/
+
